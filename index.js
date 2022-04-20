8ffaeb7c3713501624384fe5317f3a4e57ab3e7e
EddYam <edder_yamir@hotmail.com>
2022-04-19 21:10:44 -0500

    Instanciando objetos y mostrando la propiedad name de estos

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const MissionCommander = require("./app/missionCommander"); //Importa clase MissionCommander
+
+//Instanciando objetos
+const edd = new MissionCommander("Edder")
+const maria = new MissionCommander("Maria del Carmen")
+const alex = new MissionCommander("Alejandro")
+
+console.log(edd.name)
+console.log(maria.name)
+console.log(alex.name)
